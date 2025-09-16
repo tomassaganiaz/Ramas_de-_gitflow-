2c02c01c5986f42b5358bb6a85ab89802a3c0ac5
tomassaganiaz <tomassaganiaz212@gmail.com>
2025-09-15 21:37:46 -0300

    Update login.js

---
+++ Manejo_de_Ramas_en_Git/primer_ejemplo/feature/login/login.js
@@ -0,0 +1,8 @@
+// feature/login/login.js
+function login(usuario, contraseña) {
+if (usuario === "admin" && contraseña === "1234") {
+    alert("Login exitoso");
+} else {
+    alert("Credenciales incorrectas");
+}
+}
